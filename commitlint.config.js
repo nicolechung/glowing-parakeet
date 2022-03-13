cc054aa1c936f99ab5ae55bcaebb5f9f7c2b574e
nicole chung <nicole.chung@gmail.com>
2022-03-13 19:06:59 -0400

    chore: semantic (#7)

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // https://commitlint.js.org/#/reference-rules
+  // Level [0..2]: 0 disables the rule. For 1 it will be considered a warning for 2 an error.
+  // Applicable always|never: never inverts the rule.
+  // Value: value to use for this rule.
+  rules: {
+    // Let people use caps
+    'header-case': [0],
+    // Let people write  sentences
+    'header-full-stop': [0],
+    'subject-case': [0],
+  },
+};
